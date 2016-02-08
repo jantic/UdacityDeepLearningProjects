c4aa5df6090728482a247b210e364ccc878b8471
Jason <jsa169@gmail.com>
2016-02-07 20:26:55 -0800

    Generalizing network generation so that I can easily change hyperparameters.

--- 3 - Regularization/Main.py
+++ 3 - Regularization/Main.py
@@ -6,30 +6,65 @@ import tensorflow as tf
 
 pickle_file = '/home/citnaj/Desktop/tensorflow/tensorflow/tensorflow/examples/udacity/notMNIST.pickle'
 
-image_size = 28
-num_labels = 10
-train_subset = 10000
-batch_size = 128
-num_hidden_params = 1024 # 1st layer num features
-num_inputs = image_size * image_size # MNIST data input
-learningRate = 0.05
-num_steps = 3001
+_imageSize = 28
+_numLabels = 10
+_trainSubset = 10000
+_batchSize = 128
+_numHiddenFeatures = 1024
+_numHiddenLayers = 1
+_numInputs = _imageSize * _imageSize
+_learningRate = 0.05
+_numSteps = 3001
 
 def accuracy(predictions, labels):
     return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))/predictions.shape[0])
 
-def multilayerNetwork(_X, _weights, _biases):
-    hidden1 = tf.nn.relu(tf.matmul(_X, _weights['h1']) + _biases['h1'])
-    outputLayer = tf.matmul(hidden1, _weights['out']) + _biases['out']
+def validateNumHiddenLayers(numHiddenLayers):
+    if numHiddenLayers < 1:
+        raise ValueError('Number of hidden layers must be >= 1')
+
+def multilayerNetwork(inputs, weights, biases, numHiddenLayers):
+    validateNumHiddenLayers(numHiddenLayers)
+
+    hiddenLayer = tf.nn.relu(tf.matmul(inputs, weights['h1']) + biases['h1'])
+
+    for layerNum in xrange(numHiddenLayers+1):
+        if layerNum > 1:
+            previousLayer = hiddenLayer
+            hiddenLayer = tf.nn.relu(tf.matmul(previousLayer, weights['h' + str(layerNum)]) + biases['h' + str(layerNum)])
+
+    outputLayer = tf.matmul(hiddenLayer, weights['out']) + biases['out']
     return outputLayer
 
 def reformat(dataset, labels):
-    dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+    dataset = dataset.reshape((-1, _imageSize * _imageSize)).astype(np.float32)
     # Map 2 to [0.0, 1.0, 0.0 ...], 3 to [0.0, 0.0, 1.0 ...]
-    labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+    labels = (np.arange(_numLabels) == labels[:, None]).astype(np.float32)
     return dataset, labels
 
+def generateWeights(numHiddenLayers, numHiddenFeatures, numInputs, numLabels):
+    validateNumHiddenLayers(numHiddenLayers)
+    weights = {}
+    weights['h1'] = tf.Variable(tf.truncated_normal([numInputs, numHiddenFeatures]))
 
+    for layerNum in xrange(numHiddenLayers+1):
+        if layerNum > 1:
+            weights['h' + str(layerNum)] = tf.Variable(tf.truncated_normal([numHiddenFeatures, numHiddenFeatures]))
+
+    weights['out'] = tf.Variable(tf.truncated_normal([numHiddenFeatures, numLabels]))
+    return weights
+
+def generateBiases(numHiddenLayers, numHiddenFeatures, numLabels):
+    validateNumHiddenLayers(numHiddenLayers)
+    biases = {}
+    biases['h1'] = tf.Variable(tf.zeros([numHiddenFeatures]))
+
+    for layerNum in xrange(numHiddenLayers+1):
+        if layerNum > 1:
+            biases['h' + str(layerNum)] = tf.Variable(tf.zeros([numHiddenFeatures]))
+
+    biases['out'] = tf.Variable(tf.zeros([numLabels]))
+    return biases
 
 with open(pickle_file, 'rb') as f:
     save = pickle.load(f)
@@ -59,44 +94,31 @@ graph = tf.Graph()
 with graph.as_default():
     # Input data. For the training data, we use a placeholder that will be fed
     # at run time with a training minibatch.
-    tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, num_inputs))
-    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(_batchSize, _numInputs))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(_batchSize, _numLabels))
     tf_valid_dataset = tf.constant(valid_dataset)
     tf_test_dataset = tf.constant(test_dataset)
 
-    # Variables.
-    weights = {
-        'h1':tf.Variable(tf.truncated_normal([num_inputs, num_hidden_params])),
-        'out':tf.Variable(tf.truncated_normal([num_hidden_params, num_labels]))
-    }
-
-    biases = {
-        'h1':tf.Variable(tf.zeros([num_hidden_params])),
-        'out':tf.Variable(tf.zeros([num_labels]))
-    }
-    # Training computation.
-
-    trainingNetwork = multilayerNetwork(tf_train_dataset, weights, biases)
+    weights = generateWeights(_numHiddenLayers, _numHiddenFeatures, _numInputs, _numLabels)
+    biases = generateBiases(_numHiddenLayers, _numHiddenFeatures, _numLabels)
+    trainingNetwork = multilayerNetwork(tf_train_dataset, weights, biases, _numHiddenLayers)
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(trainingNetwork, tf_train_labels))
+    optimizer = tf.train.GradientDescentOptimizer(_learningRate).minimize(loss)
 
-    # Optimizer.
-    optimizer = tf.train.GradientDescentOptimizer(learningRate).minimize(loss)
-
-    # Predictions for the training, validation, and test data.
     train_prediction = tf.nn.softmax(trainingNetwork)
-    valid_prediction = tf.nn.softmax(multilayerNetwork(tf_valid_dataset, weights, biases))
-    test_prediction = tf.nn.softmax(multilayerNetwork(tf_test_dataset, weights, biases))
+    valid_prediction = tf.nn.softmax(multilayerNetwork(tf_valid_dataset, weights, biases, _numHiddenLayers))
+    test_prediction = tf.nn.softmax(multilayerNetwork(tf_test_dataset, weights, biases, _numHiddenLayers))
 
 with tf.Session(graph=graph) as session:
     tf.initialize_all_variables().run()
     print "Initialized"
-    for step in xrange(num_steps):
+    for step in xrange(_numSteps):
         # Pick an offset within the training data, which has been randomized.
         # Note: we could use better randomization across epochs.
-        offset = (np.random.randint(1, train_subset) * batch_size) % (train_labels.shape[0] - batch_size)
+        offset = (np.random.randint(1, _trainSubset) * _batchSize) % (train_labels.shape[0] - _batchSize)
         # Generate a minibatch.
-        batch_data = train_dataset[offset:(offset + batch_size), :]
-        batch_labels = train_labels[offset:(offset + batch_size), :]
+        batch_data = train_dataset[offset:(offset + _batchSize), :]
+        batch_labels = train_labels[offset:(offset + _batchSize), :]
         # Prepare a dictionary telling the session where to feed the minibatch.
         # The key of the dictionary is the placeholder node of the graph to be fed,
         # and the value is the numpy array to feed to it.
